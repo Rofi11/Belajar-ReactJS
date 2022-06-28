1ffb33555f80f0b8b1c46be96616de47737a90d3
Rofi11 <rofimuhamad0381@gmail.com>
2022-06-28 10:21:41 +0700

    mengubah tampilan depan dengan hello world

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const helloWorld = <h1>percobaan merender buatan sendiri</h1>
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+  // helloWorld
+  <h1>hello world</h1> // jsx ini dianggapnya
 );
 
 // If you want to start measuring performance in your app, pass a function
