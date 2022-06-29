04693cff5bb049eb5a71b2c65907e0b52d061250
Rofi11 <rofimuhamad0381@gmail.com>
2022-06-29 20:01:09 +0700

    dynamic component

---
+++ src/component/YutubeComp/YutubeComp.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+import './YutubeComp.css'
+
+// stateless component
+const YutubeComp = (props) => {
+    return (
+      <div className="container">
+        <div className="yutube-wrapper">
+          <div className="img-thumb">
+              <img src="../public/logo192.png"></img>
+              <p className="time">{props.time}</p>
+          </div>
+            <p className="title">{props.title}</p>
+            <p className="desc">{props.desc}</p>
+        </div>
+      </div>
+    );
+}
+
+// membuat default props nya agar kalo data dari sana nya kosong maka akan menggunakan default props nya
+  // dibuat jadi object
+YutubeComp.defaultProps = {
+  time: "00.00",
+  title: "title here",
+  desc: "xx di tonton dalam xx hari"
+}
+
+export default YutubeComp;
---
+++ src/container/Home.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import YutubeComp from '../component/YutubeComp/YutubeComp';
+
+// statefull component karena pakai class
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <YutubeComp
+                    time="7.12"
+                    title="tutorial bagian 1"
+                    desc="2x tonton dalam 2 hari"/>
+                <YutubeComp 
+                    time="6.18"
+                    title="tutorial bagian 2"
+                    desc="200x tonton dalam 4 hari"/>
+                <YutubeComp 
+                    time="3.14"
+                    title="tutorial bagian 3"
+                    desc="100x tonton dalam 7 hari"/>
+                <YutubeComp/>
+            </div>
+        )
+    }
+}
+
+export default Home;
