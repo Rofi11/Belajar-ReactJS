4f215a4e0905b02442487e3739bb974f5201cc3f
Rofi11 <rofimuhamad0381@gmail.com>
2022-07-07 12:17:30 +0700

    menambahkan coding utk fitur delete, belum jalan karena pakai api publik bukan local, fokus coding nya saja

--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -2,6 +2,7 @@
 import React, { Component,Fragment } from 'react';
 import Post from '../../component/Post/Post';
 import './BlogPost.css'
+import axios from 'axios';
 
 
 class Blogpost extends Component {
@@ -9,15 +10,33 @@ class Blogpost extends Component {
     state = {
         post: []
     }
+
+    getPostApi = () => {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then((result) => {
+                this.setState({
+                    post: result.data
+                })
+            })
+    }
+
     // 2.diubah state nya degan ComponentDidMount
     componentDidMount () {
         // tangkap API publik nya
-        fetch('https://jsonplaceholder.typicode.com/posts')
-            .then(response => response.json())
-            .then(json => {
-                this.setState({
-                    post:json
-                })
+        // fetch('https://jsonplaceholder.typicode.com/posts')
+        //     .then(response => response.json())
+        //     .then(json => {
+        //         this.setState({
+        //             post:json
+        //         })
+        //     })
+        this.getPostApi()
+    }
+
+    handleRemove = (data) => {
+        axios.delete(`https://jsonplaceholder.typicode.com/posts/${data}`)
+            .then((res) => {
+                this.getPostApi()
             })
     }
 
@@ -27,7 +46,7 @@ class Blogpost extends Component {
                 <p className='section-title'>BlogPost</p>
                 {
                     this.state.post.map(post => {
-                        return <Post key={post.id} title={post.title} desc={post.body}/>
+                        return <Post key={post.id} data={post} remove={this.handleRemove}/>
                     })
                 }
                 
